a7b4d3419a925b3c78e266ed9497cbae62f72fc4
judagonsa <judagonsa8@gmail.com>
2024-05-15 09:17:39 -0500

    Create project

---
+++ learningSwiftUI/learningSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  learningSwiftUI
+//
+//  Created by Julian González on 15/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ learningSwiftUI/learningSwiftUI/learningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningSwiftUIApp.swift
+//  learningSwiftUI
+//
+//  Created by Julian González on 15/05/24.
+//
+
+import SwiftUI
+
+@main
+struct learningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
