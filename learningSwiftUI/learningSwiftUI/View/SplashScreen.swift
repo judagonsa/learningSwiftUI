649eb0ac3e79176daf6f2b94846e2ef0b0f6471c
judagonsa <judagonsa8@gmail.com>
2024-05-15 14:18:07 -0500

    Create splash screen

---
+++ learningSwiftUI/learningSwiftUI/View/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  learningSwiftUI
+//
+//  Created by Julian González on 15/05/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
